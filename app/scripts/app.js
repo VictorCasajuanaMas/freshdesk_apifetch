ba69729754474b4e0d0825d22fa985d64792a505
Victor Casajuana Mas <victor@visionwin.com>
2023-10-03 12:40:43 +0200

    Añadidos console.log

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,45 +1,62 @@
+// version 1.0.1
+
 var client;
 
 init();
 
 async function init() {
+  console.log('#apifetch#0 init')
   client = await app.initialized();
+  console.log('#apifetch#1 cliente.events.on')
   client.events.on('app.activated', renderText);
 }
 
 async function renderText() {
   
+  console.log('#apifetch#2 start function renderText()')
   // get element from html to update -------------------
+  console.log('#apifetch#3 get apptext from document.getelementbyid("apptext")')
   const textElement = document.getElementById('apptext');
-
+  
   // get installation parameters ------------------------
+  console.log('#apifetch#4 get iparams from client.iparams.get()')
   const iparams = await client.iparams.get();
-
+  
   // get method from installation parameters ----------
-    const parameters = {
+  console.log('#apifetch#5 define const parameters');
+  const parameters = {
     method: iparams.method
   }
   
   // update waiting text on widget ---------------------
+  console.log('#apifetch#6 update waiting text on widget');
   textElement.innerText = iparams.waiting_text
   
   // get contact data ----------------------------------
+  console.log('#apifetch#7 get contact data from client.data.get("contact")');
   const contactData = await client.data.get('contact');
   
   // Take params from URL -------------------------------
+  console.log('#apifetch#8 Take iparams.url');
   const url = await new URL(iparams.url);
   var newUrl = iparams.url;
 
+  console.log('#apifetch#9 take searchParams from url')
   const params = await url.searchParams;
   
+  console.log('#apifetch#10 iterate params to replace variables')
   params.forEach((value) => {
     var variable = ''
     variable = value.replace( /[{}]/g, '')
     newUrl = newUrl.replace( value, contactData.contact[variable])
   });
 
+  console.log('#apifetch#11 newUrl: ' + newUrl)
+
   // Take params from body -------------------------------
   if ( iparams.method == 'POST' ) {
+
+    console.log('#apifetch#12 Configure POST fetch')
 
     body = iparams.body
 
@@ -74,12 +91,17 @@ async function renderText() {
   }
 
   // get response from URL ------------------------------
+  console.log('#apifetch#13 get response from URL')
   const response = await fetch(newUrl, parameters)
 
   // get response text ----------------------------------
+  console.log('#apifetch#14 get response text')
   const respuesta = await response.text();
 
   // update text on widget ------------------------------
+  console.log('#apifetch#15 update text on widget')
   textElement.innerText = respuesta
+
+  console.log('#apifetch#16 end function RenderText()')
 
 }
